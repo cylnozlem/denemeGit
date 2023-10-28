2f5d787bbd8d1b097df2c59753f5e274d458645e
cylnozlem <ceylanozlem90@gmail.com>
2023-10-29 00:58:08 +0300

    .

--- tilesExample/src/main/java/com/example/tilesExample/controller/TilesController.java
+++ tilesExample/src/main/java/com/example/tilesExample/controller/TilesController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class TilesController {
 
 	@GetMapping("/welcome")
-	public String firstPage() {
+	public String HomePage() {
 		return "welcome";
 	}
 	
